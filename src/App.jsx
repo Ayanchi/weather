8f904d8537b7e97fa41141475338571139141672
ayana <ayanausub13@gmail.com>
2023-02-13 19:54:15 +0600

    aya

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,45 @@ import React from 'react'
 import Header from './Header'
 
 
+
 const api = {
   key: '51bdc8ed39ada837dff98ec2fa3a6b32',
-  base: 'http://api.openweathermap.org/data/2.5/'
+  base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App(props) {
-  const [weather, setWeather] = useState('')
+
+  let state =  {
+    days: []
+  }
+  const [weather, setWeather] = useState([])
 
   let city = ''
 
-  useEffect(() => {
-
-  }, [setWeather])
-
 
   async function getWeather(){
-    let responce = await fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
+    let responce = await fetch(`${api.base}forecast?q=${city}&units=metric&appid=${api.key}`)
     let data = await responce.json()  
-    setWeather(data)
-    
+    const dailyData = data.list.filter(reading => reading.dt_txt.includes("12:00:00"))
+    setWeather({days: dailyData})
+    console.log(dailyData)
+    console.log(data)
   }
 
   function setCity(e) {
     city = e.target.value
   }
+
+  useEffect(() => {
+
+  }, [setWeather])
+
+  function formatCards() {
+    return state.days.map((day, index) => <Header day={day} key={index}/>)
+  }
   
   return (
     <div className="App">
-      
       <div className="first">
         <div className="wether">ПОГОДА</div>
         <div className="placeWather"></div>
@@ -41,16 +51,14 @@ function App(props) {
           type = 'text'/>
         <button onClick={getWeather}>запусти</button>
       </div>
-
-      <div className='revue'>
+      <div className="revue">
         <div className="city">
-          {weather?.name}
+          {props.weather?.main?.temp}
         </div>
         <div className="degree">
-          {weather?.main?.temp}
         </div>
       </div>
-      <Header city={city} weather={weather} setWeather={setWeather}/>
+      {formatCards()}
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,28 +5,11 @@ import React from 'react'
 
 function Header(props){
 
-  const [weekWeather, setWeekWeather]  = useState('')
-
-  useEffect(() => {
-
-  }, [setWeekWeather])
-
-  async function getWeekWeather(){
-    let resp = await fetch(`${api.base}forecast?q${props.city}&metric&APPID=${api.key}`)
-    let data = await resp.json()
-    const dailyData = data.list.filter(reading => reading.dt_txt.includes("12:00:00"))
-    setWeekWeather({days: dailyData})
-    console.log(data)
-  }
-
-  const ms = props.day * 1000
-  const weekdayName = new Date(ms).toLocaleString('ru', {weekday: 'long'})
-
   /*formatCards = () => {
     return this.state.days.map(day, index)
   }*/
 
-  const currentDate = new Date().toLocaleString("en", {
+  /* const currentDate = new Date().toLocaleString("en", {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
@@ -34,14 +17,25 @@ function Header(props){
   })
   const curentDay = new Date().toLocaleString("en", {
     weekday: 'long'
-  })
+  })*/
   
+
+  const ms = props.day.dt * 1000;
+  const weekdayName = new Date(ms).toLocaleString('ru', {weekday: 'long'});
   return(
     <div className='allWeeks'>
 
       <div className="week">
         <div className="day">
-          {curentDay}
+          {weekdayName}
+        </div>
+        <div className="degreeDay">
+          {props.day?.main?.temp}
+        </div>
+      </div>
+      <div className="week">
+        <div className="day">
+          {}
         </div>
         <div className="degreeDay">
           {}
@@ -49,34 +43,26 @@ function Header(props){
       </div>
       <div className="week">
         <div className="day">
-          {getWeekWeather}
+          {}
         </div>
         <div className="degreeDay">
-          {curentDay}
+          {}
         </div>
       </div>
       <div className="week">
         <div className="day">
-          {curentDay}
+          {}
         </div>
         <div className="degreeDay">
-          {props.weather?.main?.temp}
+          {}
         </div>
       </div>
       <div className="week">
         <div className="day">
-          {curentDay}
+          {}
         </div>
         <div className="degreeDay">
-          {props.weather?.main?.temp}
-        </div>
-      </div>
-      <div className="week">
-        <div className="day">
-          {curentDay}
-        </div>
-        <div className="degreeDay">
-          {props.weather?.main?.temp}
+          {}
         </div>
       </div>
     </div>
